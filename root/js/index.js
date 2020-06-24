02a4c25acf79399e505a3e48bd910b2acab7eb10
albertzoom11 <52132982+albertzoom11@users.noreply.github.com>
2020-06-23 20:58:54 -0400

    basic index.html set up

---
+++ root/js/index.js
@@ -0,0 +1,7 @@
+const number = document.getElementById('number');
+const northOrSouth = document.getElementById('nOrS');
+const localOrExpress = document.getElementById('lOrE');
+
+console.log(number);
+console.log(northOrSouth);
+console.log(localOrExpress);
